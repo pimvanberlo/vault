28401eaeceafba2a9c28ec96397bcaca776bb64a
Unknown <pimmetos@hotmail.com>
2019-01-22 11:34:26 +0100

    Css / Times wrong & right counter added

--- index.php
+++ index.php
@@ -8,24 +8,26 @@
     <title></title>
 </head>
 <body>
+<h1 id="unlockMessage">Enter the right code to unlock the vault.</h1>
 <div id="buttonContainer">
-    <p id="unlockMessage">Enter the right code to unlock the vault.</p>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">1</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">2</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">3</button>
-    <br>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">4</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">5</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">6</button>
-    <br>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">7</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">8</button>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">9</button>
-    <br>
     <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">0</button>
 </div>
-<div id="correctVaultColor"></div>
-<div id="wrongVaultColor"></div>
-<script src="js/vault.js">  </script>
+
+<p id="amountRight" class="rightWrongText"></p>
+<p id="amountWrong" class="rightWrongText"></p>
+<div class="colorContainer">
+    <div id="correctVaultColor"></div>
+    <div id="wrongVaultColor"></div>
+</div>
+<script src="js/vault.js"></script>
 </body>
 </html>
--- js/vault.js
+++ js/vault.js
@@ -1,9 +1,14 @@
-var unlockCode = "0492";
-var pressedButton = "";
-var buttons = document.getElementsByClassName("vaultButton");
+let unlockCode = "0492";
+let pressedButton = "";
+let buttons = document.getElementsByClassName("vaultButton");
 let wrongColor = document.getElementById("wrongVaultColor");
 let correctColor = document.getElementById("correctVaultColor");
 let intervalTimer = 0;
+let amountWrong = 0;
+let amountRight = 0;
+
+document.getElementById("amountRight").innerHTML = "Right amount: " + amountRight;
+document.getElementById("amountWrong").innerHTML = "Wrong amount: " + amountWrong;
 
 function buttonPressedKey(pressedNumber) {
     pressedButton = pressedButton + pressedNumber.innerText;
@@ -12,32 +17,45 @@ function buttonPressedKey(pressedNumber) {
     if (pressedButton.length === unlockCode.length) {
 
         if (pressedButton === unlockCode) {
+            // audio player
+            let audio = new Audio('sounds/vaultUnlock.wav');
+            audio.play();
+            // right amount counter
+            amountRight++;
+            document.getElementById("amountRight").innerHTML = "Right amount: " + amountRight;
+            // unlock message
             document.getElementById("unlockMessage").innerHTML = "Vault Unlocked!";
-            for (var i = 0; i < buttons.length; i++) {
+
+            for (var i = 0; i < buttons.length; i++) { // for loop
+
                 buttons[i].disabled = true;
+                buttons[i].style.opacity = "0.6";
+                buttons[i].style.cursor = "not-allowed";
 
             }
-            let blink = setInterval(function() {
+
+            let blink = setInterval(function () {
 
                 //add +1 every time the setInterval function runs
                 intervalTimer++;
-
                 //change the css of the green and red box to create a blinking effect
                 console.log(intervalTimer);
                 if (correctColor.style.backgroundColor === "green") {
                     correctColor.style.backgroundColor = "rgb(0, 70, 0)";
-                }else {
+                } else {
                     correctColor.style.backgroundColor = "green";
+
                 }
 
-                //check if the interval has ran ten times
-                if(intervalTimer === 8) {
+                //check if the interval has ran 8 times
+                if (intervalTimer === 8) {
                     //ClearInterval function stops the interval after 8 times
                     clearInterval(blink);
                     intervalTimer = 0;
                     for (var i = 0; i < buttons.length; i++) {
                         buttons[i].disabled = false;
-
+                        buttons[i].style.opacity = "1";
+                        buttons[i].style.cursor = "pointer";
                     }
                 }
 
@@ -45,33 +63,38 @@ function buttonPressedKey(pressedNumber) {
 
         }
         else {
+            let audio = new Audio('sounds/wrongCode.wav');
+            audio.play();
+
+            amountWrong++;
+            document.getElementById("amountWrong").innerHTML = "Wrong amount: " + amountWrong;
+
             document.getElementById("unlockMessage").innerHTML = "Wrong Code!";
             for (var i = 0; i < buttons.length; i++) {
                 buttons[i].disabled = true;
+                buttons[i].style.opacity = "0.6";
+                buttons[i].style.cursor = "not-allowed";
 
             }
-
-
-            let blink = setInterval(function() {
-
+            let blink = setInterval(function () {
                 //add +1 every time the setInterval function runs
                 intervalTimer++;
-
                 //change the css of the green and red box to create a blinking effect
                 console.log(intervalTimer);
                 if (wrongColor.style.backgroundColor === "red") {
                     wrongColor.style.backgroundColor = "rgb(70, 0, 0)";
-                }else {
+                } else {
                     wrongColor.style.backgroundColor = "red";
                 }
-
                 //check if the interval has ran ten times
-                if(intervalTimer === 8) {
+                if (intervalTimer === 8) {
                     //ClearInterval function stops the interval after 8 times
                     clearInterval(blink);
                     intervalTimer = 0;
                     for (var i = 0; i < buttons.length; i++) {
                         buttons[i].disabled = false;
+                        buttons[i].style.opacity = "1";
+                        buttons[i].style.cursor = "pointer";
                     }
                 }
 
