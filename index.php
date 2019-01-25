b34bab337a09214a15be56f4e17198bc5125c02d
Unknown <pimmetos@hotmail.com>
2019-01-25 22:39:28 +0100

    Vault animations / css

--- index.php
+++ index.php
@@ -8,25 +8,32 @@
     <title></title>
 </head>
 <body>
-<h1 id="unlockMessage">Enter the right code to unlock the vault.</h1>
-<div id="buttonContainer">
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">1</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">2</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">3</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">4</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">5</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">6</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">7</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">8</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">9</button>
-    <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">0</button>
-</div>
-
-<p id="amountRight" class="rightWrongText"></p>
-<p id="amountWrong" class="rightWrongText"></p>
-<div class="colorContainer">
-    <div id="correctVaultColor"></div>
-    <div id="wrongVaultColor"></div>
+<h1>Enter the right code to unlock the vault.</h1>
+<div class="container">
+    <h2 id="unlockMessage"></h2>
+    <div id="buttonContainer">
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">1</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">2</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">3</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">4</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">5</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">6</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">7</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">8</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">9</button>
+        <button class="vaultButton" type="button" onclick="buttonPressedKey(this)">0</button>
+    </div>
+    <div class="rightWrongContainer">
+        <p id="amountRight" class="rightWrongText"></p>
+        <p id="amountWrong" class="rightWrongText"></p>
+    </div>
+    <div class="colorContainer">
+        <div id="correctVaultColor"></div>
+        <div id="wrongVaultColor"></div>
+    </div>
+    <img id="key" src="pictures/key.png" alt="Vault" height="100" width="200">
+    <img id="vault" src="pictures/vault.png" alt="Key" height="200" width="200">
+    <img id="blocker" src="pictures/block.png" alt="Blocker" height="150" width="150">
 </div>
 <script src="js/vault.js"></script>
 </body>
--- js/vault.js
+++ js/vault.js
@@ -3,9 +3,13 @@ let pressedButton = "";
 let buttons = document.getElementsByClassName("vaultButton");
 let wrongColor = document.getElementById("wrongVaultColor");
 let correctColor = document.getElementById("correctVaultColor");
+let keyUnlock = document.getElementById("key");
+let vaultUnlock = document.getElementById("vault");
+let unlockBlocker = document.getElementById("blocker");
 let intervalTimer = 0;
 let amountWrong = 0;
 let amountRight = 0;
+let position = 0;
 
 document.getElementById("amountRight").innerHTML = "Right amount: " + amountRight;
 document.getElementById("amountWrong").innerHTML = "Wrong amount: " + amountWrong;
@@ -25,17 +29,33 @@ function buttonPressedKey(pressedNumber) {
             document.getElementById("amountRight").innerHTML = "Right amount: " + amountRight;
             // unlock message
             document.getElementById("unlockMessage").innerHTML = "Vault Unlocked!";
+            // car animation\
+            var move = setInterval(frame);
 
+            function frame() {
+                if (position === 1410) {
+                    position = 0;
+                    clearInterval(move);
+                    keyUnlock.style.visibility = "hidden";
+                    keyUnlock.style.left = position + "px"
+                }
+                else {
+                    position++;
+                    keyUnlock.style.left = position + "px";
+                    keyUnlock.style.visibility = "visible";
+                    unlockBlocker.style.visibility = "hidden";
+                }
+            }
+
+            // for loop for the button disabler
             for (var i = 0; i < buttons.length; i++) { // for loop
 
                 buttons[i].disabled = true;
                 buttons[i].style.opacity = "0.6";
                 buttons[i].style.cursor = "not-allowed";
-
             }
 
             let blink = setInterval(function () {
-
                 //add +1 every time the setInterval function runs
                 intervalTimer++;
                 //change the css of the green and red box to create a blinking effect
@@ -44,11 +64,9 @@ function buttonPressedKey(pressedNumber) {
                     correctColor.style.backgroundColor = "rgb(0, 70, 0)";
                 } else {
                     correctColor.style.backgroundColor = "green";
-
                 }
-
                 //check if the interval has ran 8 times
-                if (intervalTimer === 8) {
+                if (intervalTimer === 10) {
                     //ClearInterval function stops the interval after 8 times
                     clearInterval(blink);
                     intervalTimer = 0;
@@ -58,23 +76,40 @@ function buttonPressedKey(pressedNumber) {
                         buttons[i].style.cursor = "pointer";
                     }
                 }
-
             }, 500);
-
         }
         else {
-            let audio = new Audio('sounds/wrongCode.wav');
-            audio.play();
-
+            // wrong amount counter
             amountWrong++;
             document.getElementById("amountWrong").innerHTML = "Wrong amount: " + amountWrong;
-
+            // wrong code text
             document.getElementById("unlockMessage").innerHTML = "Wrong Code!";
+            // animation
+            move = setInterval(block);
+
+            function block() {
+                if (position === 1200) {
+                    position = 0;
+                    clearInterval(move);
+                    keyUnlock.style.visibility = "hidden";
+                    unlockBlocker.style.visibility = "visible";
+                    keyUnlock.style.left = position + "px";
+                    // audio player
+                    let audio = new Audio('sounds/wrongCode.wav');
+                    audio.play();
+                }
+                else {
+                    position++;
+                    keyUnlock.style.left = position + "px";
+                    keyUnlock.style.visibility = "visible";
+                    unlockBlocker.style.visibility = "hidden";
+                }
+            }
+            // for loop for the button disabler
             for (var i = 0; i < buttons.length; i++) {
                 buttons[i].disabled = true;
                 buttons[i].style.opacity = "0.6";
                 buttons[i].style.cursor = "not-allowed";
-
             }
             let blink = setInterval(function () {
                 //add +1 every time the setInterval function runs
@@ -87,7 +122,7 @@ function buttonPressedKey(pressedNumber) {
                     wrongColor.style.backgroundColor = "red";
                 }
                 //check if the interval has ran ten times
-                if (intervalTimer === 8) {
+                if (intervalTimer === 10) {
                     //ClearInterval function stops the interval after 8 times
                     clearInterval(blink);
                     intervalTimer = 0;
@@ -97,7 +132,6 @@ function buttonPressedKey(pressedNumber) {
                         buttons[i].style.cursor = "pointer";
                     }
                 }
-
             }, 500);
         }
         pressedButton = "";
